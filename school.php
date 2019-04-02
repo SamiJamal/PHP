cc79ddc1915faae298f41628a3360bbde584b12b
SamiJamal <42804026+SamiJamal@users.noreply.github.com>
2019-04-02 09:41:02 +0200

    Add files via upload

---
+++ school.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 11-2-2019
+ * Time: 09:17
+ */
+
+$user = "Schooluser";
+$pass = "school";
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=school; port=3306', $user, $pass);
+    foreach($dbh->query('SELECT * from cursist') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+
+
